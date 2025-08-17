2e2cc2f216f1831d356a77e1a87324d3df9ef28e
Shourav Paul <paul.shourav01@gmail.com>
2025-08-18 01:26:15 +0600

    Intial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,56 @@
+import "@/src/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@heroui/link";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/src/config/site";
+import { fontSans } from "@/src/config/fonts";
+import { Navbar } from "@/src/components/navbar";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen text-foreground bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Providers themeProps={{ attribute: "class",  }}>
+          <div className="relative flex flex-col h-screen">
+           
+            <main className="container mx-auto max-w-7xl  flex-grow">
+              {children}
+            </main>
+           
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
